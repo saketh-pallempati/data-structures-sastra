fea343a85c515d32fd6ba7db9ad200a0373ed215
Saketh Pallempati <113260794+saketh-pallempati@users.noreply.github.com>
2023-08-23 15:35:53 +0530

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+void bubble(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+void selection(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+void insertion(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > current)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+}
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    cout << "Enter length of the array" << endl;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int x;
+    cout << "Enter \n1 for Bubble \n2 for Selection \n3 for Insertion" << endl;
+    cin >> x;
+    switch (x)
+    {
+    case 1:
+        bubble(arr, n);
+        break;
+    case 2:
+        selection(arr, n);
+        break;
+    case 3:
+        insertion(arr, n);
+        break;
+    default:
+        break;
+    }
+    print(arr, n);
+    return 0;
+}
